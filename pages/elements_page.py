8e193bc89689829c6d72144140922f1bb5b5292c
Dmitry Zadnepryansky <d.zadnepryansky@gmail.com>
2022-12-17 18:00:24 +0200

    returned and printed: full_name, email, current_address, permanent_address

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -8,8 +8,16 @@ class TextBoxPage(BasePage):
     locators = TextBoxPageLocators()
 
     def fill_all_fields(self):
-        self.element_is_visible(self.locators.FULL_NAME).send_keys('Dmytro')
-        self.element_is_visible(self.locators.EMAIL).send_keys('d.zadnepryansky@gmail.com')
-        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys('Chernivtsi')
-        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys('Kharkiv')
+        self.element_is_visible(self.locators.FULL_NAME).send_keys('dima')
+        self.element_is_visible(self.locators.EMAIL).send_keys('fgy@gmail.com')
+        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys('kharkov')
+        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys('kiev')
         self.element_is_visible(self.locators.SUBMIT).click()
+
+    def check_filled_form(self):
+        full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text.split(':')[1]
+        email = self.element_is_present(self.locators.CREATED_EMAIL).text.split(':')[1]
+        current_address = self.element_is_present(self.locators.CREATED_CURRENT_ADDRESS).text.split(':')[1]
+        permanent_address = self.element_is_present(self.locators.CREATED_PERMANENT_ADDRESS).text.split(':')[1]
+        return full_name, email, current_address, permanent_address
+
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -10,4 +10,10 @@ class TestElements:
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
             text_box_page.fill_all_fields()
-            time.sleep(5)
+            output_name, output_email, output_curr_addr, output_per_addr = text_box_page.check_filled_form()
+
+            print(output_name)
+            print(output_email)
+            print(output_curr_addr)
+            print(output_per_addr)
+
